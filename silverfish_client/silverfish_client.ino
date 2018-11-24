42a2ee2ee255d5ce79ee65872077ca9354c92f01
Alex Brooke <akb.sbc@gmail.com>
2018-11-23 18:11:14 -0600

    test driving

--- silverfish_client/silverfish_client.ino
+++ silverfish_client/silverfish_client.ino
@@ -1,49 +1,29 @@
+// system includes
+#include <string>
+#include <vector>
+
+// "system" includes
 #include <AWS_IOT.h>
-//#include <aws_iot_config.h>
-//#include <aws_iot_error.h>
-//#include <aws_iot_json_utils.h>
-//#include <aws_iot_log.h>
-//#include <aws_iot_mqtt_client.h>
-//#include <aws_iot_mqtt_client_common_internal.h>
-//#include <aws_iot_mqtt_client_interface.h>
-//#include <aws_iot_shadow_actions.h>
-//#include <aws_iot_shadow_interface.h>
-//#include <aws_iot_shadow_json.h>
-//#include <aws_iot_shadow_json_data.h>
-//#include <aws_iot_shadow_key.h>
-//#include <aws_iot_shadow_records.h>
-//#include <aws_iot_version.h>
-
-// esp32 SDK includes JSMN so no need to use AWS_IOT's version
-// #include <jsonParser.h>
-
-//#include <network_interface.h>
-//#include <network_platform.h>
-//#include <threads_interface.h>
-//#include <threads_platform.h>
-//#include <timer_interface.h>
-//#include <timer_platform.h>
-
-#include <ESP32_Servo.h>
-
+#include <jsmn.h>
 #include <WiFi.h>
 
 // contains WIFI_SSID, WIFI_PASSWORD, and HOST_ADDR
 #include "secrets.h"
-
+// chassis
 #include "Chassis.hpp"
 
 AWS_IOT silverfish;
 
 char CLIENT_ID[] = "silverfish";
-char TOPIC_NAM[] = "$aws/things/silverfish/shadow/update";
 
 int status = WL_IDLE_STATUS;
 
-Chassis chassis(22, 23);
+Chassis chassis(23, 22);
 
 void handle_forward(char*, int, char* payload) {
-  // TODO
+  // TODO: get power, duration from payload
+  chassis.forward(80, 1000);
+  chassis.stop();
 }
 
 void handle_reverse(char*, int, char* payload) {
@@ -81,10 +61,11 @@ void setup() {
   }
   Serial.println("Done");
   delay(5000);
-  silverfish.subscribe((char*)"robot/drive/forward", test_sub);
-  silverfish.subscribe((char*)"robot/drive/reverse", test_sub);
-  silverfish.subscribe((char*)"robot/turn/cw", test_sub);
-  silverfish.subscribe((char*)"robot/turn/ccw", test_sub);
+  silverfish.publish((char*)"robot/status", (char*)"{\"ready\":true}");
+  silverfish.subscribe((char*)"robot/drive/forward", handle_forward);
+//  silverfish.subscribe((char*)"robot/drive/reverse", test_sub);
+//  silverfish.subscribe((char*)"robot/turn/cw", test_sub);
+//  silverfish.subscribe((char*)"robot/turn/ccw", test_sub);
 }
 
 void loop() {
