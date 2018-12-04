6037d5b84610e543c9081a0f04c386ecec26383a
Alex Brooke <akb.sbc@gmail.com>
2018-12-04 00:02:57 -0500

    update lambda to properly handle SNS event

--- silverfish-intermediary/index.js
+++ silverfish-intermediary/index.js
@@ -4,24 +4,28 @@ AWS.config.Region = 'us-east-1';
 const iotdata = new AWS.IotData({ endpoint: 'acobcyfsx523y-ats.iot.us-east-2.amazonaws.com' });
 
 exports.handler = async (event) => {
-    switch (event.action) {
+    console.log(event.Records[0].Sns);
+    for (let r of event.Records) {
+        const msg = JSON.parse(r.Sns.Message);
+        switch (msg.action) {
         case 'drive':
-            console.log(`driving at ${event.payload.power} for ${event.payload.duration}ms`)
+            console.log(`driving at ${msg.payload.power} for ${msg.payload.duration}ms`)
             await iotdata.publish({
                 topic: 'robot/drive',
-                payload: JSON.stringify(event.payload),
+                payload: JSON.stringify(msg.payload),
                 qos: 0
             }).promise();
             break;
         case 'turn':
-            if (!event.direction) return { statusCode: 400, message: 'must specify a direction!' }
-            if (event.direction !== 'cw' && event.direction !== 'ccw') return { statusCode: 400, message: `invalid direction ${event.direction}!` }
+            if (!msg.direction) return { statusCode: 400, message: 'must specify a direction!' }
+            if (msg.direction !== 'cw' && msg.direction !== 'ccw') return { statusCode: 400, message: `invalid direction ${msg.direction}!` }
             await iotdata.publish({
-                topic: `robot/turn/${event.direction}`,
-                payload: JSON.stringify(event.payload),
+                topic: `robot/turn/${msg.direction}`,
+                payload: JSON.stringify(msg.payload),
                 qos: 0
             }).promise();
             break;
+    }
     }
     return { statusCode: 200, message: 'finished successfully' }
 };
